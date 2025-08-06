6e0b3532330062de89b54833a4fc01588f2c2f5b
RK120994 <i.ramakrishna94@gmail.com>
2025-08-06 10:07:44 +0200

    Update apim.tf

--- apim.tf
+++ apim.tf
@@ -3,7 +3,7 @@ resource "azurerm_api_management" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   publisher_name      = "Ramakrishna"
-  publisher_email     = "Ramakrishna.Immadi@lifehealthcare.co.za"
+  publisher_email     = "xxxxxxxxxxxxx"
   sku_name            = "Developer_1"
 
   tags = {
